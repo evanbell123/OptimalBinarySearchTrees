25613023b071a62fdf10aaf07d501761da0595d9
Evan Bell <evanbell91@gmail.com>
2015-11-18 12:14:23 -0600

    constructing binary trees

---
+++ OptimalBinarySearchTrees/Entry.h
@@ -0,0 +1,68 @@
+//#include "Header.h"
+#include <queue>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Entry
+{
+private:
+	int minFrequency;
+	int minComparisons;
+	queue <int> optimalRoots;
+public:
+	Entry() : minFrequency(0), minComparisons(0) {};
+	Entry(int mWeight) : minFrequency(mWeight), minComparisons(0) {};
+	Entry(int mWeight, int mComparisons) : minFrequency(mWeight), minComparisons(mComparisons) {};
+	Entry(int mWeight, int mComparisons, int opRoot) : minFrequency(mWeight), minComparisons(mComparisons)
+	{
+		addOptimalRoot(opRoot);
+	};
+	Entry(int mWeight, int mComparisons, queue<int> opRoots) : minFrequency(mWeight), minComparisons(mComparisons)
+	{
+		swap(optimalRoots, opRoots);
+	};
+
+	int getMinFrequency() { return minFrequency; };
+	int getMinComparisons() { return minComparisons; };
+	queue <int> getOptimalRoots() { return optimalRoots; };
+
+	//return root at front of queue, then pop
+	int popOptimalRoot()
+	{
+		if (!optimalRoots.empty())
+		{
+			int root = optimalRoots.front();
+			optimalRoots.pop();
+			return root;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	void setMinFrequency(int mWeight) { minFrequency = mWeight; };
+	void setMinComparisons(int mComparisons) { minComparisons = mComparisons; };
+	void setOptimalRoots(queue<int> opRoots) { swap(optimalRoots, opRoots); };
+
+
+	void addOptimalRoot(int root) { optimalRoots.push(root); };
+	void Entry::print(int width)
+	{
+		queue <int> temp = optimalRoots;
+		cout << setw(width) << '|'
+			<< "F = " << minFrequency << endl
+			<< setw(width) << '|'
+			<< "C = " << minComparisons << endl
+			<< setw(width) << '|'
+			<< "R = ";
+		while (!temp.empty())
+		{
+			cout << temp.front() << " ";
+			temp.pop();
+		}
+		cout << endl;
+	};
+
+};
--- OptimalBinarySearchTrees/Source.cpp
+++ OptimalBinarySearchTrees/Source.cpp
@@ -1,12 +1,13 @@
+#include "OBSTComputationTable.h"
+#include "BinaryTreeConstruction.h"
 
-#include "OBSTComputationTable.cpp"
-#include "Node.cpp"
-
-#include <fstream>
+//#include <fstream>
+//#include <string>
 
 void displayFrequencies(queue<int> frequencies);
 
-Node *constructOBST(LookupTable &table, int row, int column);
+BinaryTree *constructOBST(LookupTable &table, int row, int column);
+
 
 void main()
 {
@@ -43,6 +44,10 @@ void main()
 	int j = static_cast<int>(table.getTotalFrequencies());
 	LookupTable lookupTable = table.getLookupTable();
 
+	BinaryTree *obst(constructOBST(lookupTable, i, j));
+
+	printPretty(obst, 1, 0, cout);
+
 	system("pause");
 }
 
@@ -58,10 +63,22 @@ void displayFrequencies(queue<int> frequencies)
 
 //row must be initialized to 1
 //column must be initialized to the total number of frequencies
-Node *constructOBST(LookupTable &table, int row, int column)
+
+BinaryTree *constructOBST(LookupTable &table, int row, int column)
 {
+	BinaryTree *OBST;
 	if (row != column)
 	{
-		return constructOBST(table, row - 1, column);
+		int optimalRoot = table[Key(row, column)].popOptimalRoot();
+		//int leftOptimalRoot = table[Key(row, optimalRoot-1)].popOptimalRoot();
+		//int rightOptimalRoot = table[Key(optimalRoot+1, column)].popOptimalRoot();
+		OBST = new BinaryTree(optimalRoot);
+		OBST->left = constructOBST(table, row, optimalRoot - 1);
+		OBST->right = constructOBST(table, optimalRoot + 1, column);
 	}
+	else
+	{
+		OBST = NULL;
+	}
+	return OBST;
 }
