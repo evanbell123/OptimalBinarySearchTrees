6bf923b912b9f53b5f82ff6e40b9099dd9a63bb0
evanbell123 <evanbell123@yahoo.com>
2015-11-19 07:15:52 -0600

    construct and display binary tree

--- OptimalBinarySearchTrees/Source.cpp
+++ OptimalBinarySearchTrees/Source.cpp
@@ -25,7 +25,7 @@ void main()
 			getline(myfile, line, ','); //get next frequency
 			line.erase(0, 1); // remove the space
 			frequency = stoi(line, &stringSize);
-			if (frequency != 0)
+			if (frequency != 0) 
 			{
 				frequencies.push(frequency); // add to queue
 			}
@@ -44,9 +44,22 @@ void main()
 	int j = static_cast<int>(table.getTotalFrequencies());
 	LookupTable lookupTable = table.getLookupTable();
 
-	BinaryTree *obst(constructOBST(lookupTable, i, j));
+	BinaryTree *obst1 = constructOBST(lookupTable, i, j);
+	BinaryTree *obst2 = constructOBST(lookupTable, i, j);
+	printPretty(obst1, 1, 0, cout);
+	printPretty(obst2, 1, 0, cout);
 
-	printPretty(obst, 1, 0, cout);
+	/*
+	BinaryTree *root = new BinaryTree(2);
+	root->left = new BinaryTree(1);
+	root->right = new BinaryTree(4);
+	root->right->left = new BinaryTree(3);
+	root->right->right = new BinaryTree(5);
+
+	printPretty(root, 1, 0, cout);
+	*/
+
+	
 
 	system("pause");
 }
@@ -63,22 +76,15 @@ void displayFrequencies(queue<int> frequencies)
 
 //row must be initialized to 1
 //column must be initialized to the total number of frequencies
-
 BinaryTree *constructOBST(LookupTable &table, int row, int column)
 {
-	BinaryTree *OBST;
+	int optimalRoot = table[Key(row, column)].popOptimalRoot();
+	BinaryTree *OBST = new BinaryTree(optimalRoot);;
 	if (row != column)
 	{
-		int optimalRoot = table[Key(row, column)].popOptimalRoot();
-		//int leftOptimalRoot = table[Key(row, optimalRoot-1)].popOptimalRoot();
-		//int rightOptimalRoot = table[Key(optimalRoot+1, column)].popOptimalRoot();
 		OBST = new BinaryTree(optimalRoot);
 		OBST->left = constructOBST(table, row, optimalRoot - 1);
 		OBST->right = constructOBST(table, optimalRoot + 1, column);
-	}
-	else
-	{
-		OBST = NULL;
 	}
 	return OBST;
 }
